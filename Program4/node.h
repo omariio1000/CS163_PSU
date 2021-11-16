5050d1d4efb75cee5bad88a6610136977857d481
Omar Nassar <omariio1000@gmail.com>
2021-11-15 17:08:07 -0800

    started program 4

---
+++ Program4/node.h
@@ -0,0 +1,50 @@
+/* Omar Nassar
+ * Portland State University CS163
+ * November 15, 2021
+ * Node class to hold information for vehicles for Binary Search Tree
+ */
+
+#ifndef node_h
+#define node_h
+
+#include <cstring>
+#include <cctype>
+#include <iostream>
+#include <cstdlib>
+
+class node {
+    public:
+        //constructor and destructor
+        node();
+        ~node();
+
+        //add data functions 
+        int addData(char * inMake, char * inModel, char * inColor, char * inInfo, int inYear, int inPrice, int inMiles);
+        int addData(node * inData);
+
+        //display node information
+        int display();
+
+        //display price
+        int displayPrice();
+
+        //compare data vs arg node's data (greater or lower)
+        bool compare(node * comparing);
+
+        //children nodes
+        node * left;
+        node * right;
+
+    private:
+        //data being stored
+        char * make;
+        char * model;
+        char * color;
+        char * info;
+
+        int year;
+        int price;
+        int miles;
+};
+
+#endif
---
+++ Program4/tree.h
@@ -0,0 +1,52 @@
+/* Omar Nassar
+ * Portland State University CS163
+ * November 15, 2021
+ * Binary Search Tree used to store cars and their information
+ */
+
+#ifndef tree_h
+#define tree_h
+
+#include "node.h"
+
+#include <cstring>
+#include <cctype>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+
+class tree {
+    public:
+        //constructor and destructor
+        tree();
+        ~tree();
+
+        //wrapper to add vehicle to tree
+        int addVehicle(node * inData);
+
+        //wrapper to retrieve matching vehicles
+        int retrieve(char * make, char * model, int year, node **& retrieving);
+
+        //wrapper to remove matching vehicles
+        int remove(char * make, char * model, int year);
+
+        //wrapper to find height of tree
+        int height();
+        
+        //wrapper to find efficiency of tree
+        int efficiency();
+
+    private:
+        //recursive functions
+        int addVehicle(node * root, node * inData);
+        int retrieve(char * make, char * model, int year, node**& retrieving, node * root);
+        int remove(char * make, char * model, int year);
+        int height(node * root);
+        int efficiency(node * root);
+
+        node * root;
+
+};
+
+#endif
