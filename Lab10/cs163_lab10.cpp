adc40de7bd7a8a4f1e70d8ffda3baa12455feae5
Omar Nassar <omariio1000@gmail.com>
2021-12-02 13:03:22 -0800

    finished lab 10

--- Lab10/cs163_lab10.cpp
+++ Lab10/cs163_lab10.cpp
@@ -8,6 +8,22 @@ int main()
     trees.display();
 
     /*  PLACE YOUR FUNCTION CALL HERE */
+    trees.display_largest();
+    cout << endl << "The root's value is repeated " << trees.count_root() << " times in the BST" << endl;
+
+    /*table copyTree;
+    copyTree.copy(trees);
+    cout << endl << "THIS IS THE NEW TREE" << endl;
+    copyTree.display();*/
+
+    cout << endl << "The number of nodes with 3 data members in the 2-3-4 tree is " << trees.count_3_data() << endl;
+    cout << endl << "The height of the 2-3-4 tree is " << trees.height() << endl;
+
+    table copyTree;
+    copyTree.copy(trees);
+    copyTree.copy_234(trees);
+    cout << endl << "NEW TREE" << endl;
+    copyTree.display();
 
     return 0;
 }
